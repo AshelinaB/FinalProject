0c9afe6e5b04051b0f8f0a7aceb18d24fd5c86c4
Ashley <ashley.broadbelt@gmail.com>
2017-05-09 23:06:24 -0400

    add

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Brooklyn Shred Locations</title>
-	<link rel= "stylesheet" href= "style.css">
-</head>
-<body>
-<h1>Locations</h1>
-<table></table>
-	<tr>
-		<th> Sponsered By</th>
-		<th> Borough</th>
-		<th> Time </th>
-
-	</tr>
-</body>
-</html>
+  
